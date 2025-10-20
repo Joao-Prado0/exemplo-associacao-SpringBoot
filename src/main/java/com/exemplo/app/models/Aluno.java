bc1b3f971eeb88be1ec2ff23ccfc2d9dd81ae4cd
Joao Prado <joaopradocampos@gmail.com>
2025-10-20 11:43:02 -0300

    feat: Implementação relacionamento classe Aluno.java

--- src/main/java/com/exemplo/app/models/Aluno.java
+++ src/main/java/com/exemplo/app/models/Aluno.java
@@ -11,5 +11,8 @@ import lombok.Setter;
 
 public class Aluno extends Usuario {
 
-
+    // relacionamento 1:N com Curso
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_curso", nullable = false, foreignKey = @ForeignKey(name = "fk_usuario_curso"))
+    private Curso curso;
 }
